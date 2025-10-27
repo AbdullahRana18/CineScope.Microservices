47af02b99369f8a58527159cc8b8a3f4c17e69cf
Abdullah Rana <the.abdullah.1829@gmail.com>
2025-10-28 01:09:35 +0500

    Initial commit

---
+++ PNDCProject/AuthService/Program.cs
@@ -0,0 +1,24 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Enable OpenAPI + Swagger UI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure middleware pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(); // 👈 ye line Swagger UI enable karti hai
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
